8ed06571c76ba24c7e5dcd7039d2386dd393c926
Nick Bernhardt <nicholasjbernhardt@gmail.com>
2012-12-31 15:16:30 -0600

    Added template HTML

--- beerlist.php
+++ beerlist.php
@@ -1,3 +1,203 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Bernie's Tap Room</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/reset.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="css/grid.css" type="text/css" media="screen">
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+  <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,400italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Vidaloka' rel='stylesheet' type='text/css'> 
+    <script src="js/jquery-1.6.3.min.js" type="text/javascript"></script>
+    <script src="js/superfish.js" type="text/javascript"></script>
+    <script src="js/FF-cash.js" type="text/javascript"></script> 
+    <script src="js/script.js" type="text/javascript"></script>  
+	<script src="js/jquery.hoverIntent.js" type="text/javascript"></script>
+    <script src="js/jScrollPane.js" type="text/javascript"></script>
+    <script src="js/jquery.mousewheel.js" type="text/javascript"></script>
+    <script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+	<script type="text/javascript" src="js/tms-0.3.js"></script>
+    <script type="text/javascript" src="js/tms_presets.js"></script>
+    <script src="js/bgstretcher.js" type="text/javascript"></script>
+    <script type="text/javascript">
+		$(document).ready(function(){
+			//  Initialize Backgound Stretcher
+				$('BODY').bgStretcher({
+				images: ['images/bg4.jpg'],
+				slideShow:                  false
+			});
+		});
+	</script> 
+    <script type="text/javascript">
+		$(window).load(function() {
+			$('.slider')._TMS({
+				duration:800,
+				easing:'easeOutQuart',
+				preset:'simpleFade',
+				slideshow:30000,
+				banners:false,
+				pauseOnHover:true,
+				waitBannerAnimation:false,
+				prevBu:false,
+				nextBu:false,
+				pagination: '.paggination'
+			});
+		});
+   </script> 
+   
+	<!--[if lt IE 8]>
+        <div style=' clear: both; text-align:center; position: relative;'>
+            <a href="http://www.microsoft.com/windows/internet-explorer/default.aspx?ocid=ie6_countdown_bannercode"><img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0"  alt="" /></a>
+        </div>
+	<![endif]-->
+    <!--[if lt IE 9]>
+   		<script type="text/javascript" src="js/html5.js"></script>
+        <link rel="stylesheet" href="css/ie.css" type="text/css" media="screen">
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic' rel='stylesheet' type='text/css'> 
+	<![endif]-->
+    
+    
+    <script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-35043825-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+    
+    
+</head>
+<body id="page4">
+	<div class="extra">
+        <!--==============================header=================================-->
+        <header>
+            <div class="main">
+            	<div class="container_12 relative">
+                    <div class="menu-box">
+                        	<div class="menu-bot">
+                            	<div class="menu-mid">
+                                	<div class="menu-top">
+                                    	<div class="menu-padd">
+                                        	<div class="top-line">
+                                            	<div class="top-line-bg">
+                                                	Follow us on  <a class="decor" href="http://www.facebook.com/BerniesTapRoom"target=â€_blankâ€>Facebook</a>
+                                                </div>
+                                            </div>
+                                            <h1><a href="index.html">brewery</a></h1>
+                                            <nav>
+                                                <ul class="menu">
+                                                    <li><a href="Home.html">Home</a></li>
+                                                    <li><a href="Beers.html">Our beers</a></li>
+                                                    <li><a class="current" href="Menu.html">Menu</a></li>
+                                                    <li><a href="Pictures.html">Photos</a></li>
+                                                    <li><a href="Specials.html">Events and Specials</a></li>
+                                                    <li><a href="Contact.html">Contacts</a></li>
+                                                </ul>
+                                            </nav>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </header>
+        <!--===================================content========================================-->
+        <section id="content">
+        	<div class="main">
+            	<div class="container_12">
+                	<div class="wrapper">
+                    	<div class="grid_9 prefix_3">
+                        	<div class="container">
+                            	<div class="slider">
+                                	<ul class="items">
+                                        <li>
+                                            <img src="images/1.jpg" alt="" />
+                                        </li>
+                                        <li>
+                                            <img src="images/2.jpg" alt="" />
+                                        </li>
+                                        <li>
+                                            <img src="images/3.jpg" alt="" />
+                                        </li>
+                                        <li>
+                                            <img src="images/4.jpg" alt="" />
+                                        </li>
+                                        <li>
+                                            <img src="images/5.jpg" alt="" />
+                                        </li>
+                                        <li>
+                                            <img src="images/6.jpg" alt="" />
+                                        </li>
+                                    </ul>
+                            		<ul class="paggination">
+                                        <li><a href="#">1</a></li>
+                                        <li><a href="#">2</a></li>
+                                        <li><a href="#">3</a></li>
+                                        <li><a href="#">4</a></li>
+                                        <li><a href="#">5</a></li>
+                                        <li><a href="#">6</a></li>
+                                    </ul>
+                                    <div class="clear"></div>
+                                </div>
+                                <div class="inside-padd">
+                                	<div class="content-1">
+                                        <div class="scroll-pane">
+                                            <div class="content-padd">
+                                            <center>
+                                     <h3 class="indent-bot2">Add Beer</h3>
+
+
+                                      
+                                 </center>               
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        	<div class="block-1"></div>
+        </section>
+        </div>
+        <!--==============================footer=================================-->
+        <footer>
+            <div class="main">
+            	<div class="container_12">
+                	<div class="wrapper">
+                    	<div class="grid_12">
+                        	<div class="footer-padd">
+                            	<div class="wrapper">
+                            		<span class="footer-links"><span class="color-1">Bernie's Tap Room</span> &copy; 2012
+                                </div>
+                            </div>
+                        </div>	
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <script type="text/javascript">
+   		$(window).load(function() { 
+		$('.scroll-pane').jScrollPane();
+	  }); 
+
+   </script>
+</body>
+</html>
+
+
 <?php
 
 /*
